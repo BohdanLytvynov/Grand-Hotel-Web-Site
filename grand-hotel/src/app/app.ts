a0e6c6883bce8179842ca34f719894ca10faacc6
Bohdan Lytvynov <blytv@softserveinc.com>
2025-08-04 00:15:07 +0300

    header about us done

--- grand-hotel/src/app/app.ts
+++ grand-hotel/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./navbar/navbar";
+import { Header } from "./header/header";
+import { AboutUs } from "./about-us/about-us";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Header, AboutUs],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
