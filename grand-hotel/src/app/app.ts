eb1fd9ed69b33e8e0f3e0ed137ba2f4789b0e417
Bohdan Lytvynov <blytv@softserveinc.com>
2025-08-05 23:43:13 +0300

    Customers markup added

--- grand-hotel/src/app/app.ts
+++ grand-hotel/src/app/app.ts
@@ -4,10 +4,11 @@ import { Navbar } from "./navbar/navbar";
 import { Header } from "./header/header";
 import { AboutUs } from "./about-us/about-us";
 import { Rooms } from "./rooms/rooms";
+import { Customers } from "./customers/customers";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Header, AboutUs, Rooms],
+  imports: [RouterOutlet, Navbar, Header, AboutUs, Rooms, Customers],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ grand-hotel/src/app/customers/customers.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonHeader } from "../common-header/common-header";
+
+@Component({
+  selector: 'app-customers',
+  imports: [CommonHeader],
+  templateUrl: './customers.html',
+  styleUrl: './customers.css'
+})
+export class Customers {
+
+}
