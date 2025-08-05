5f4e997ee711f3272be7d560de3358029afb6853
Bohdan Lytvynov <blytv@softserveinc.com>
2025-08-05 15:49:39 +0300

    rooms markup added

--- grand-hotel/src/app/app.ts
+++ grand-hotel/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./navbar/navbar";
 import { Header } from "./header/header";
 import { AboutUs } from "./about-us/about-us";
+import { Rooms } from "./rooms/rooms";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Header, AboutUs],
+  imports: [RouterOutlet, Navbar, Header, AboutUs, Rooms],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
